3e014f4704b9dfcef880cbab347e6b4d7d144e45
Frank-4a5a <449758@edu.rocmn.nl>
2019-08-02 17:22:51 +0200

    search toegevoegd

--- View/HomeView.php
+++ View/HomeView.php
@@ -1,4 +1,4 @@
-<p>Overzicht Pagina</p>
+<h3>Overzicht</h3>
 <div class="row">
 <table class="col-4">
   <tr>
@@ -57,7 +57,19 @@
   <tr>
     <td class="mergecell" colspan="10"></td>
   </tr>
+  <tr>
+    <td class="mergecell searchForm" colspan="10">
+      <form class="" action="/" method="">
+        <span class="col-1"></span>
+        <input type="text" name="" value="">
+        <button type="submit" name="">
+          <i class="fas fa-search"></i>
+        </button>
+      </form>
+    </td>
+  </tr>
 </table>
+
 <table class="col-4">
   <tr>
     <td colspan="2"></td>
